413ef9838130ddbfa238c56e171dcd7f5adcd4a2
Adrian Lazzi <alazzi23@students.polytechnic.org>
2022-01-11 09:28:38 -0800

    Added the die class

---
+++ Craps.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Craps here.
+ *
+ * @author Adrian Lazzi
+ * @version 01-09-2022
+ */
+import java.util.Scanner;
+public class Craps
+{
+    public static void main(String[] args)
+    {
+        
+        
+    }
+}
